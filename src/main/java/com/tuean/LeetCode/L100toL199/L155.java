e5584631646e6cc94dc21afc5143bb2ecce71212
tuean <tuean_z@163.com>
2019-11-07 18:27:05 +0800

    commit

--- src/main/java/com/tuean/LeetCode/L100toL199/L155.java
+++ src/main/java/com/tuean/LeetCode/L100toL199/L155.java
@@ -7,28 +7,28 @@ public class L155 {
 //    设计一个最小堆
 
     /** initialize your data structure here. */
-    private static Stack stack;
-    private static int mininum = Integer.MIN_VALUE;
-
-    public L155() { // MinStack
-        stack = new Stack();
-    }
-
-    public void push(int x) {
-        stack.push(x);
-        mininum = Math.min(x, mininum);
-    }
-
-    public void pop() {
-
-    }
-
-    public int top() {
-
-    }
-
-    public int getMin() {
-
-    }
+//    private static Stack stack;
+//    private static int mininum = Integer.MIN_VALUE;
+//
+//    public L155() { // MinStack
+//        stack = new Stack();
+//    }
+//
+//    public void push(int x) {
+//        stack.push(x);
+//        mininum = Math.min(x, mininum);
+//    }
+//
+//    public void pop() {
+//
+//    }
+//
+//    public int top() {
+//
+//    }
+//
+//    public int getMin() {
+//
+//    }
 
 }
--- src/main/java/com/tuean/LeetCode/L1toL99/L39.java
+++ src/main/java/com/tuean/LeetCode/L1toL99/L39.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class L39 {
 
     // 从数组中找到所有和为target的可能
-    //
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
